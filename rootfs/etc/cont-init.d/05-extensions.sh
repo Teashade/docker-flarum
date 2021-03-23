4a6fc6b39bc639dd68107098cb8c1e0c4ff72aac
Raymond Luo <rymdluo@users.noreply.github.com>
2021-03-23 13:45:16 +0800

    Create 05-extensions.sh

---
+++ rootfs/etc/cont-init.d/05-extensions.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/with-contenv bash
+
+echo "Adding custom extensions..."
+mkdir -p /data/assets /data/extensions/.cache /data/storage
+touch /data/extensions/list
+
+# Auto-add extensions
+echo $'\n\
+andre-pullinen/ads\n\
+migratetoflarum/fake-data\n\
+fof/default-group\n\
+fof/recaptcha\n\
+AlexanderOMara/flarum-gravatar\n\
+fof/socialprofile\n\
+fof/share-social\n\
+fof/nightmode\n\
+therealsujitk/flarum-ext-gifs\n\
+zerosonesfun/elint\n\
+fof/pretty-mail\n\
+fof/realtimelogin\n\
+fof/realtimedate\n\
+nearata/flarum-ext-tags-color-generator\n\
+therealsujitk/flarum-ext-hljs\n\
+fof/drafts\n\
+fof/gamification\n\
+askvortsov/flarum-discussion-templates\n\
+fof/split\n\
+fof/merge-discussions\n\
+fof/formatting\n\
+dem13n/topic-starter-label\n\
+fof/best-answer\n\
+flarumite/simple-discussion-views\n\
+fof/impersonate\n\
+fof/masquerade\n\
+fof/disposable-emails\n\
+fof/stopforumspam\n\
+fof/filter\n\
+fof/html-errors\n\
+fof/links\n\
+davwheat/custom-sidenav-links\n\
+fof/frontpage\n\
+askvortsov/flarum-categories\n\
+dem13n/discussion-cards\n\
+fof/pages\n\
+fof/custom-footer\n\
+fof/terms\n\
+fof/analytics\n\
+fof/byobu\n\
+askvortsov/flarum-pwa\n\
+v17development/flarum-blog\n\
+v17development/flarum-seo\n\
+blomstra/payments'> /data/extensions/list
+
+if [ -s "/data/extensions/list" ]; then
+  while read extension; do
+    test -z "${extension}" && continue
+    extensions="${extensions}${extension} "
+  done < /data/extensions/list
+  echo "Installing additional custom extensions..."
+  COMPOSER_CACHE_DIR="/data/extensions/.cache" su-exec flarum:flarum composer config repositories.blomstra composer https://extiverse.com/composer/
+  COMPOSER_CACHE_DIR="/data/extensions/.cache" su-exec flarum:flarum composer config --global --auth bearer.extiverse.com ${EXTIVERSE_TOKEN}
+  COMPOSER_CACHE_DIR="/data/extensions/.cache" su-exec flarum:flarum composer require --working-dir /opt/flarum ${extensions}
+fi
+
+su-exec flarum:flarum php flarum migrate
+su-exec flarum:flarum php flarum cache:clear
